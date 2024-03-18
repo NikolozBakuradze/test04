a94c7f6694eb1057959efc13cac8518120d6e441
NikolozBakuradze <162832780+NikolozBakuradze@users.noreply.github.com>
2024-03-18 10:45:52 +0400

    Add files via upload

---
+++ java02/src/p1/C1.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class C1 {
+    public static String MESSAGE = "This is C1 message";
+
+    public static void main(String[] args) {
+        System.out.println(C1.MESSAGE);
+        System.out.println(C2.MESSAGE);
+        System.out.println(C11.MESSAGE);
+        System.out.println(C3.MESSAGE);
+    }
+}
